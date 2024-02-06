e6017241c3bdd94b0c7a8a58538b835f4ba8ee4a
Jonathan Andrews <andrews.jonathanw@gmail.com>
2024-02-05 19:36:25 -0600

    formatting done for signup/in clerk pages

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -5,7 +5,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Navbar() {
 
   return (
-    <div className="sticky top-0">
+    <div className="sticky top-0 ">
       <UserButton afterSignOutUrl="/" />
     </div>
   )
