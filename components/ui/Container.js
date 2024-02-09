333dc5fad1a2652fad4a14de86eb308b23db63f4
Jonathan Andrews <andrews.jonathanw@gmail.com>
2024-02-09 13:58:59 -0600

    Refactor UI styles in page.js, Button.js, and Container.js

--- components/ui/Container.js
+++ components/ui/Container.js
@@ -1,10 +1,11 @@
 const Container = ({ children }) => {
   return (
-    <div className="max-w-[1920px] w-full mx-auto xl:px-20 p-4">
+    <div className="max-w-[1920px] w-full mx-auto xl:px-20 p-4 fixed z-50 top-0 border border-b-primary/10 bg-secondary">
       {children}
     </div>
   );
 };
 
 export default Container;
+
 
