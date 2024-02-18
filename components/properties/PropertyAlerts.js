be419a916e2461c6181413de6e7d1d6c196f5cfd
Jonathan Andrews <andrews.jonathanw@gmail.com>
2024-02-18 17:31:12 -0600

    Add PropertyAlerts component to PropertyCard

---
+++ components/properties/PropertyAlerts.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
+import { BsPersonXFill,BsPersonFillCheck } from "react-icons/bs";
+
+export default function PropertyAlerts({ property }) {
+  const hasUnits = property.units && property.units.length > 0;
+  const vacantUnitsCount = hasUnits ? property.units.filter(unit => !unit.occupied).length : 0;
+
+  return (
+    <div className='absolute top-0 left-0 p-2'>
+      {hasUnits ? (
+        <div className='flex items-center justify-center'>
+          {vacantUnitsCount > 0 ? (
+            <>
+              <BsPersonXFill className='text-yellow-500' />
+              <span className='ml-1 text-sm text-white'>{`${vacantUnitsCount} Vacancies`}</span>
+            </>
+          ) : (
+            <>
+              <BsPersonFillCheck className='text-green-600' />
+              <span className='ml-1 text-sm text-white'>No Vacancy</span>
+            </>
+          )}
+        </div>
+      ) : (
+        <div className={`flex items-center justify-center text-${property.occupied ? 'green' : 'yellow'}-600`}>
+          {property.occupied ? <BsPersonFillCheck className='text-green-600' /> : <BsPersonXFill className='text-yellow-600' />}
+          <span className='ml-1 text-sm text-white'>{property.occupied ? 'Occupied' : 'Vacant'}</span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+
--- components/properties/PropertyCard.js
+++ components/properties/PropertyCard.js
@@ -2,10 +2,14 @@
 import React from 'react'
 import Image from 'next/image'
 import CardToolBar from './CardToolBar' // Adjust the import path as necessary
+import PropertyAlerts from './PropertyAlerts'
 
 export default function PropertyCard({ property }) {
   return (
     <div className={`propertyCard relative w-full min-h-[280px] min-w-[280px] overflow-hidden my-4 group`}>
+      <div className="relative z-20"> {/* Ensure the alert has a higher z-index */}
+        <PropertyAlerts property={property} />
+      </div>
       <Image
         src={property.imageUrl}
         className='rounded-2xl'
@@ -13,6 +17,7 @@ export default function PropertyCard({ property }) {
         layout="fill"
         objectFit="cover"
       />
+
       <div className=" absolute bottom-0 left-0 p-4 bg-black bg-opacity-50 w-full rounded-b-2xl">
         <h2 className="text-slate-100 text-lg font-bold">{property.name}</h2>
         <p className="text-slate-100">{property.address}</p>
