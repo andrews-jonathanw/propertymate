af6bf5022650706f741dafece5b5321e1e705675
Jonathan Andrews <andrews.jonathanw@gmail.com>
2024-03-02 03:12:13 -0600

    Add sharp package and update PropertyCard.js

--- components/properties/PropertyCard.js
+++ components/properties/PropertyCard.js
@@ -1,27 +1,30 @@
-// PropertyCard.js
-import React from 'react'
-import Image from "next/image"
-import CardToolBar from './CardToolBar' // Adjust the import path as necessary
-import PropertyAlerts from './PropertyAlerts'
+import React from 'react';
+import Image from 'next/image';
+import CardToolBar from './CardToolBar';
+import PropertyAlerts from './PropertyAlerts';
 
-export default function PropertyCard({ property, onView, setScrollPosition}) {
+export default function PropertyCard({ property, onView, setScrollPosition }) {
   return (
-    <div className={`propertyCard relative w-full min-h-[280px] min-w-[280px] overflow-hidden my-4`}>
-      <div className="relative z-20"> {/* Ensure the alert has a higher z-index */}
+    <div className={`propertyCard relative w-full overflow-hidden my-4`}>
+      <div className="relative z-20">
         <PropertyAlerts property={property} />
       </div>
-      <Image
-        src={property.imageUrl}
-        className='rounded-2xl'
-        alt={property.name}
-        priority={true}
-        fill
-        sizes='(min-width: 640px) 50vw, 100vw'
-        style={{
-          objectFit: "cover"
-        }} />
+      <div style={{ position: 'relative', height: '280px' }}>
+        <Image
+          src={property.imageUrl}
+          className='rounded-2xl'
+          alt={property.name}
+          fill
+          sizes='(min-width: 640px) 50vw, 100vw'
+          style={{
+            objectFit: 'cover'
+          }}
+          fetchpriority="high"
+          priority
+        />
+      </div>
 
-      <div className=" absolute bottom-0 left-0 p-4 bg-black bg-opacity-50 w-full rounded-b-2xl">
+      <div className="absolute bottom-0 left-0 p-4 bg-black bg-opacity-50 w-full rounded-b-2xl">
         <h2 className="text-slate-100 text-lg font-bold">{property.name}</h2>
         <p className="text-slate-100">{property.address}</p>
       </div>
@@ -29,5 +32,3 @@ export default function PropertyCard({ property, onView, setScrollPosition}) {
     </div>
   );
 }
-
-
